74870c7e564335f6f4bd549c73aabb9205d093da
leesoorim <xjsksks@naver.com>
2025-06-26 16:36:50 +0900

    수림

--- erp/src/main/java/com/erp/entity/MaterialDto.java
+++ erp/src/main/java/com/erp/entity/MaterialDto.java
@@ -1,12 +1,19 @@
-package com.erp.entity;
+package com.myerp.entity;
 
+import java.time.LocalDateTime;
 import java.util.Date;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,47 +21,48 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name="MATERIAL")
+@Table(name = "MATERIAL")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class MaterialDto {
-
-	// 자재 ID
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int matid;
-
-	// 자재명
-	@Column(nullable = false, length = 100)
-	private String matnm;	
+@EntityListeners(AuditingEntityListener.class) // ★ 중요!
+	public class MaterialDto {
 	
-	// 자재코드
-	@Column(nullable = false, unique = true, length = 50)
-	private String matcd;  
+		@Id
+	    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "matid_seq")
+	    @SequenceGenerator(name = "matid_seq", sequenceName = "matid_seq", initialValue = 1000001, allocationSize = 1)
+	    @Column(nullable = false)
+	    private int matid;
 
-	// 자재 단위
-	@Column(length = 50)
-	private String matun;
+	    @Column(nullable = false, length = 100)
+	    private String matnm;
 
-	// 자재 단가
-	@Column(nullable = false)
-	private int matuprc;  
+	    @Column(nullable = false, length = 100, unique = true)
+	    private String matcd;
 
-	// 현재 재고 수량
-	@Column(nullable = false)
-	private int matstq; 
+	    @Column(nullable = false, length = 50)
+	    private String matun;
 
-	// 자재 등록일
-	private Date matcrdt;
+	    @Column(nullable = false)
+	    private int matuprc;
 
-	// 자재 분류 (카테고리)
-	@Column(length = 100)
-	private String matcate;
+	    @Column
+	    private int matstq;
 
-	// 비고
-	@Column(length = 255)
-	private String matnote;
+	    @CreatedDate
+	    @Column(updatable = false)
+	    private LocalDateTime matcrdt; // 등록일
+
+	    @LastModifiedDate
+	    private LocalDateTime matupdt; // 수정일
+
+	    @Column(nullable = false, length = 100)
+	    private String matcate;
+
+	    @Column(length = 500)
+	    private String matnote;
+	
+
 
 }
